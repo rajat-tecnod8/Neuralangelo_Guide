1ea93fed211bf62815f282b4a8d2db1b334ae87b
Rajat Vishwakarma <rajat.kumar@tecnod8.com>
2024-09-06 19:30:12 +0530

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,28 @@
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download>
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plu>
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc>
+
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+
+echo "All modules installed. Restarting docker."
+sudo systemctl restart docker
+
+echo "Running example image : Hello World"
+sudo docker run hello-world
+
